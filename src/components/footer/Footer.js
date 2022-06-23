b17f16a53a5e98cb0dbec446639ee201a5fd9fd6
Kayro França <k4yro01@gmail.com>
2022-06-23 10:11:58 -0300

    add Footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import './styles.css';
+
+function Footer() {
+    return(
+        <footer className='footerContainer'>
+            <ul className='footerMenu'>
+                <li>Quem Somos</li>
+                <li>Nossas Soluções</li>
+                <li>Nossos Projetos</li>
+                <li>Nossa Equipe</li>
+                <li>Notícias</li>
+                <li>Contato</li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
